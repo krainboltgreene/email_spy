dc17c6af42285a40d5a019e249bf48d31021b4cf
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-06-21 02:10:04 -0500

    Creating a constant with the email directory name

--- lib/email_spy.rb
+++ lib/email_spy.rb
@@ -4,6 +4,10 @@ require "cgi"
 require "uri"
 require "launchy"
 
+module EmailSpy
+  EMAIL_DIRECTORY_NAME = "email_spy"
+end
+
 require_relative "email_spy/message"
 require_relative "email_spy/delivery_method"
 require_relative "email_spy/railtie" if defined? Rails
