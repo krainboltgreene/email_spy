6488b56e59418773a848893910ca432896f24845
krainboltgreene <me@kurtisrainboltgreene.name>
2012-06-21 00:29:44 -0500

    Helping reduce size of expression with variables in railtie

--- lib/email_spy/railtie.rb
+++ lib/email_spy/railtie.rb
@@ -1,7 +1,8 @@
 module EmailSpy
   class Railtie < Rails::Railtie
     initializer "emailspy.add_delivery_method" do
-      ActionMailer::Base.add_delivery_method :email_spy, EmailSpy::DeliveryMethod, location: Rails.root.join("tmp", "email_spy")
+      path_to_tmp = Rails.root.join "tmp", "email_spy"
+      ActionMailer::Base.add_delivery_method :email_spy, EmailSpy::DeliveryMethod, location: path_to_tmp
     end
   end
 end
